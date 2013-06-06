9050555f47321e2348e531496f79f93dc1c6393e
Steven! Ragnarök <steven@nuclearsandwich.com>
2013-06-05 18:12:17 -0700

    Plan stage one.

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python2
+
+print("Would you like to play a game?")
+print("Pick a number between 1 and 100")
+raw_input("Press enter when you're ready.")
+
+print("I guess 50.")
+answer = raw_input("Is it too high or too low?")
+
+
